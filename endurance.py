27b178676956dbbabfc1b81484d8cc62cd5cc722
AntonioLegnante <AntonioLegnante12@gmail.com>
2018-12-28 13:43:29 +0100

    ingrippato

--- endurance.py
+++ endurance.py
@@ -1,17 +1,18 @@
-#Determinare la duranta massima di volo conoscendo la quantità di carburante espressa in Galloni e il consumo orario (Galloni/h)
-print ("Determina la durata massima di volo conoscendo la quantita di carbutante ed il consumo orario.")
-carburante=int (input ("carburante \t"))
+#Determinare la duranta massima di volo conoscendo la quantità di carburante espressa in Galloni e il consumo orario (Galloni/h) 
+#Soluzione alternativa che dovrebbe funzionare ma...
+print ("Determina la durata massima di volo conoscendo la quantita di carbutante ed il consumo orario.") 
+carburante=float (input ("carburante \t"))
+consumo_orario= float (input("consumo orario \t"))   
 if carburante<=0:
     print ("secondo te un aereo senza carburante può mai partire")
-consumo_orario= int (input("consumo orario \t"))
-if consumo_orario<=0:
-    print ("secondo te gli aerei come volano?")
-variabile_temporanea= carburante/consumo_orario
-ore = int (variabile_temporanea)
-variabile_temporanea=variabile_temporanea - ore
-variabile_temporanea=variabile_temporanea*60
-minuti = int (variabile_temporanea)
-variabile_temporanea=variabile_temporanea - ore
-variabile_temporanea=variabile_temporanea*60
-secondi= int (variabile_temporanea)
-print ("la massima durata del volo possibile e' ", ore , "h" , minuti , "min" , secondi, "sec")
+elif consumo_orario<=0:
+     print ("arriverai a destinazione nel 20mai!")
+else:
+    ore = int (carburante / consumo_orario)
+    carburante = carburante % consumo_orario           #modifichiamo il valore di carburante 
+    minuti = int (carburante / consumo_orario*60)
+    carburante= carburante % consumo_orario            #modifichiamo il valore di carburante
+    secondi = int (carburante / consumo_orario*60)
+    print ("la massima durata del volo possibile e' ", ore , "h" , minuti , "min" , secondi, "sec")
+#problema: nonostante ci siano i simboli di assegnazione è come se il programma non tenesse conto della prima modifica e pertanto  esegua 2 volte le stesse istruzioni
+#ps: ingrippo
