c70d0ad5a6fdaca3e68548c18a4737ba3f986d9e
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-01-02 19:52:48 +0100

    determinare il volo di un aereo avendo disponibili consumo orario e carburante

--- endurance.py
+++ endurance.py
@@ -1,5 +1,4 @@
 #Determinare la duranta massima di volo conoscendo la quantità di carburante espressa in Galloni e il consumo orario (Galloni/h) 
-#Soluzione alternativa che dovrebbe funzionare ma...
 print ("Determina la durata massima di volo conoscendo la quantita di carbutante ed il consumo orario.") 
 carburante=float (input ("carburante \t"))
 consumo_orario= float (input("consumo orario \t"))   
@@ -8,11 +7,14 @@ if carburante<=0:
 elif consumo_orario<=0:
      print ("arriverai a destinazione nel 20mai!")
 else:
-    ore = int (carburante / consumo_orario)
-    carburante = carburante % consumo_orario           #modifichiamo il valore di carburante 
-    minuti = int (carburante / consumo_orario*60)
-    carburante= carburante % consumo_orario            #modifichiamo il valore di carburante
-    secondi = int (carburante / consumo_orario*60)
+    temporanea = carburante / consumo_orario
+    ore = int (temporanea)
+    temporanea-= ore     
+    temporanea*=60 
+    minuti = int (temporanea)
+    temporanea-=minuti
+    temporanea*=60           
+    secondi = int (temporanea)
     print ("la massima durata del volo possibile e' ", ore , "h" , minuti , "min" , secondi, "sec")
-#problema: nonostante ci siano i simboli di assegnazione è come se il programma non tenesse conto della prima modifica e pertanto  esegua 2 volte le stesse istruzioni
-#ps: ingrippo
+
+
